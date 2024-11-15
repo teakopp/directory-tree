0ebf51118def4ce75addb89420b8d0a329bb5cbf
Travis <teakopp@protonmail.com>
2024-11-14 16:59:41 -0800

    test bfs implementation

--- directories.py
+++ directories.py
@@ -2,6 +2,7 @@ import sys
 
 directories = {"/": {}}
 
+
 def bfs(directory):
 	visited = set()
 	queue = [("/", directories["/"])]
@@ -17,9 +18,12 @@ def bfs(directory):
 				queue.append((name, path))
 	return None
 
-def create_directory(name, path):
-	directories[name] = {}
-	directories[name][path] = {}
+def create_directory(directory):
+	paths = directory.split("/")
+	parent = paths[:-2]
+	child = paths[-1]
+	print(parent, child)
+
 
 def remove_directory(name):
 	del directories[name]
@@ -30,6 +34,5 @@ def list_directories():
 
 directories["/"] = {"home": {"user": {}, "admin": {}}, "etc": {}, "var": {}}
 curr = bfs("user")
-print(curr)
 curr["thing"] = {}
-print(directories)
+create_directory("home/user/thing")
