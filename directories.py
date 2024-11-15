b39d3ffee60dc54c69c85789eddcc4d06aaed044
Travis <teakopp@protonmail.com>
2024-11-14 16:25:21 -0800

    test bfs implementation

--- directories.py
+++ directories.py
@@ -4,23 +4,22 @@ directories = {"/": {}}
 
 def bfs(directory):
 	visited = set()
-	queue = []
-	queue.append("/")
+	queue = [("/", directories["/"])]
 	visited.add("/")
 	while queue:
-		curr_dir = queue.pop()
+		curr_dir, curr_path = queue.pop()
 		if curr_dir == directory:
-			return curr_dir
-		print(curr_dir)
-		for name, path in directories[curr_dir].items():
-			print(name, path)
-			if path not in visited:
-				visited.add(path)
-				queue.append(path)
+			return curr_path
+		for name, path in curr_path.items():
+			print(name,path)
+			if name not in visited:
+				visited.add(name)
+				queue.append((name, path))
 	return None
 
 def create_directory(name, path):
-	directories[name] = {path}
+	directories[name] = {}
+	directories[name][path] = {}
 
 def remove_directory(name):
 	del directories[name]
@@ -29,6 +28,8 @@ def list_directories():
 	for name, path in directories.items():
 		print(name, path)
 
-directories["/"] = { "/" : {"/"}, "home": "/home", "etc": "/etc", "var": "/var"}
-directories["/"]["home"] = {"user": {"/"}, "admin": {"/"}}
+directories["/"] = {"home": {"user": {}, "admin": {}}, "etc": {}, "var": {}}
 curr = bfs("user")
+print(curr)
+curr["thing"] = {}
+print(directories)
