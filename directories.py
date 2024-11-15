b8c6030c0afe2f04a9d22c420199d33d2a614765
Travis <teakopp@protonmail.com>
2024-11-14 17:31:16 -0800

    fix paths split in _split_dir

--- directories.py
+++ directories.py
@@ -2,6 +2,13 @@ import sys
 
 directories = {"/": {}}
 
+def _split_dir(directory):
+	paths = directory.split("/")
+	print(paths[-2])
+	# If there are less than 2 paths it means that the directory is in the root
+	if len(paths) < 2:
+		return ["/", paths[-1]]
+	return paths[-2:]
 
 def bfs(directory):
 	visited = set()
@@ -12,28 +19,27 @@ def bfs(directory):
 		if curr_dir == directory:
 			return curr_path
 		for name, path in curr_path.items():
-			print(name,path)
 			if name not in visited:
 				visited.add(name)
 				queue.append((name, path))
 	return None
 
 def create_directory(directory):
-	paths = directory.split("/")
-	# If there are less than 2 paths it means that the directory is in the root
-	if len(paths) <2:
-		parent = paths[-2]
-		child = paths[-1]
-	else:
-		parent = "/"
-		child = paths
+	parent, child = _split_dir(directory)
+	print(parent, child)
 
 	curr = bfs(parent)
+	if not curr:
+		print("Parent directory does not exist")
+		return
+	
 	curr[child] = {}
 
 
-def remove_directory(name):
-	del directories[name]
+def delete_directory(directory):
+	parent, child = _split_dir(directory)
+	curr = bfs(parent)	
+	del curr[child]
 
 def list_directories():
 	for name, path in directories.items():
@@ -43,5 +49,3 @@ directories["/"] = {"home": {"user": {}, "admin": {}}, "etc": {}, "var": {}}
 curr = bfs("user")
 curr["thing"] = {}
 create_directory("home/user/yep")
-create_directory("hi")
-print(directories)
