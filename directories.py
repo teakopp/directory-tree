6efdb67a9ff07ca298b38350bbe31dc5436360b5
Travis <teakopp@protonmail.com>
2024-11-14 23:04:09 -0800

    use bfs for list_directories

--- directories.py
+++ directories.py
@@ -11,20 +11,20 @@ class DirectoryManager:
 	def __init__(self):
 		self.root = Directory("/")
 
-	def find_dir(directory):
+	def find_dir(self, directory):
 		paths = directory.split("/")[:-1]
-		curr = DirectoryManager.root
+		curr = self.root
 		for p in paths:
-			if p not in curr:
+			if p not in curr.children:
 				print(f"{p} directory does not exist")
 				return 
-			curr = curr[p]
+			curr = curr.children[p]
 		return curr
 
 	def create_directory(self, directory):
 		curr = self.find_dir(directory)
 		child = directory.split("/")[-1]
-		curr[child] = {}
+		curr.children[child] = Directory(child)
 
 	def move_directory(self, target, location):
 		self.create_directory(location)
@@ -36,11 +36,16 @@ class DirectoryManager:
 		del curr[child]
 
 	def list_directories(self):
-		for name, path in self.root.items():
-			print(name, path)
+		queue = deque()
+		queue.append(self.root)
+		while queue:
+			curr = queue.popleft()
+			print(curr.name)
+			for child in curr.children:
+				queue.append(curr.children[child])
 
 DirectoryManager = DirectoryManager()
-DirectoryManager.create_directory("/home")
-DirectoryManager.create_directory("/home/user")
-DirectoryManager.create_directory("/home/user/documents")
+DirectoryManager.create_directory("home")
+DirectoryManager.create_directory("home/user")
+DirectoryManager.create_directory("home/user/documents")
 DirectoryManager.list_directories()
