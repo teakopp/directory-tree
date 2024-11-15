832dea5ebd0fe6e2953a645cfb78c5583be3b36e
Travis <teakopp@protonmail.com>
2024-11-14 22:17:32 -0800

    no need for bfs, search is easier than that

--- directories.py
+++ directories.py
@@ -3,33 +3,23 @@ import sys
 
 directories = {"/": {}}
 
-def _split_dir(directory):
-	paths = directory.split("/")
-	# If there are less than 2 paths it means that the directory is in the root
-	if len(paths) < 2:
-		return ["/", paths[-1]]
-	return paths[-2:]
-
-def bfs(directory):
-	paths =  _split_dir(directory)
+def bfs(paths):
+	curr = directories["/"]
+	print(paths)
 	for p in paths:
-		print(p)
-	
-	return None
-def create_directory(directory):
-	parent, child = _split_dir(directory)
+		if p not in curr:
+			print(f"{p} directory does not exist")
+			return False
+		curr = curr[p]
+	return curr
 
-	print(parent, child)
-	curr = bfs(parent)
-	if not curr:
-		print("Parent directory does not exist")
-		return False
-	
+def create_directory(directory):
+	paths = directory.split("/")
+	curr = bfs(paths[:-1])
+	child = paths[-1]
 	curr[child] = {}
-	return True
 
 def move_directory(target, location):
-	target_parent, target_child = _split_dir(target)
 	created = create_directory(f"{location}/{target_child}")
 	deleted = delete_directory(target)
 	print(f"Created: {created}, Deleted: {deleted}")
@@ -53,11 +43,6 @@ def list_directories():
 		print(name, path)
 
 directories["/"] = {"home": {"user": {}, "admin": {}}, "etc": {}, "var": {}}
-curr = bfs("user")
-curr["thing"] = {}
+list_directories()
 create_directory("home/user/yep")
-list_directories()
-delete_directory("home/user/yep")
-list_directories()
-move_directory("home/user", "home/admin")
 list_directories()
